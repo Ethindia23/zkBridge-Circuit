4f59ccee816e6e2e8b799db262350968eaf5cfa1
Ashis <pradhanashish95@gmail.com>
2023-12-09 03:39:49 +0530

    added secret generation

---
+++ scripts/generatePrrof.js
@@ -0,0 +1,39 @@
+const { ethers } = require("ethers");
+const wc = require("../deposit_js/witness_calculator");
+const fs = require("fs").promises;
+
+const BNToBinary = (n) => {
+  let r = BigInt(n).toString();
+  let prePadding = "";
+  let paddingAmount = 256 - r.length;
+  for (let i = 0; i < paddingAmount; i++) {
+    prePadding += "0";
+  }
+  return prePadding + r;
+};
+
+const depositProofGenerator = async (input) => {
+  const buffer = await fs.readFile("scripts/deposit.wasm");
+  const depositWC = await wc(buffer);
+  const r = await depositWC.calculateWitness(input, 0);
+  const commitment = r[1];
+  const nullifierHash = r[2];
+  console.log(commitment);
+  console.log(nullifierHash);
+};
+const main = async () => {
+  const secret = BNToBinary(
+    ethers.BigNumber.from(ethers.utils.randomBytes(32)).toString()
+  ).split("");
+  const nullifier = BNToBinary(
+    ethers.BigNumber.from(ethers.utils.randomBytes(32)).toString()
+  ).split("");
+
+  const input = {
+    secret,
+    nullifier,
+  };
+  await depositProofGenerator(input);
+};
+
+main();
